3530f19389b9b245fce32543e6369cc9ef5c7cb7
NgHoaiTan <tannh3108@gmail.com>
2025-03-14 17:29:41 +0700

    Add Comment model

---
+++ backend/src/models/Comment.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    author: [{
+        type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true
+    }],
+    text: { type: String, required: true },
+    post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true }
+
+}, { timestamps: true })
+
+const Comment = mongoose.model('Comment', commentSchema)
+module.exports = Comment;
