236521b76a3a6f173e9e9db438ef7d7ba1c6d203
NgHoaiTan <tannh3108@gmail.com>
2025-04-01 09:20:26 +0700

    conf: add socket config

---
+++ backend/src/config/socket.js
@@ -0,0 +1,43 @@
+const { Server } = require("socket.io");
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: process.env.URL,
+        methods: ["GET", "POST"]
+    }
+});
+
+const userSocketMap = {}; // this map stores socket id corresponding the user id; userId -> socketId
+
+function getReceiverSocketId(receiverId) {
+    return userSocketMap[receiverId];
+}
+
+io.on("connection", function (socket) {
+    const userId = socket.handshake.query.userId;
+    if (userId) {
+        userSocketMap[userId] = socket.id;
+    }
+
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", function () {
+        if (userId) {
+            delete userSocketMap[userId];
+        }
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });
+});
+
+module.exports = {
+    app,
+    server,
+    io,
+    getReceiverSocketId
+};
